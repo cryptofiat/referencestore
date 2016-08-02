f5832e3b918033433b15bd3305ff6f915eaae7d8
Egon Elbre <egonelbre@gmail.com>
2016-08-02 17:18:38 +0300

    Use leveldb for storing data.

--- main.go
+++ main.go
@@ -1,35 +1,34 @@
 package main
 
 import (
-	"crypto/rand"
 	"encoding/hex"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"sync"
+
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
 var (
-	storedir = flag.String("dir", ".store", "data store directory")
-	listen   = flag.String("listen", ":8000", "listening address")
+	dbfile = flag.String("data", "data", "database directory")
+	listen = flag.String("listen", ":8000", "listening address")
 )
 
 func main() {
 	flag.Parse()
 
-	store := NewStore()
+	db, err := leveldb.OpenFile(*dbfile, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-	// Where's the right place to initiate DB vars?
-	db, err := leveldb.OpenFile(".refstore/refs.db", nil)
+	store := NewStore(db)
 
 	log.Printf("Starting server on %s\n", *listen)
 	log.Fatal(http.ListenAndServe(*listen, store))
-
-	defer db.Close()
 }
 
 const HashLength = 32
@@ -48,26 +47,12 @@ func (ref *Hash) Hex() string {
 	return hex.EncodeToString(ref[:])
 }
 
-type Blob []byte
-
-func GenerateHash() (ref Hash) {
-	_, err := io.ReadFull(rand.Reader, ref[:])
-	if err != nil {
-		panic(err)
-	}
-	return
-}
-
 type Store struct {
-	mu    sync.Mutex
-	blobs map[Hash]Blob
+	DB *leveldb.DB
 }
 
-func NewStore() *Store {
-	return &Store{
-		mu:    sync.Mutex{},
-		blobs: make(map[Hash]Blob),
-	}
+func NewStore(db *leveldb.DB) *Store {
+	return &Store{db}
 }
 
 func (store *Store) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -83,21 +68,13 @@ func (store *Store) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		store.mu.Lock()
-		blob, ok := store.blobs[txhash]
-		store.mu.Unlock()
-
-		//alternative db implementation
-		//txhash is effectively [Hashlength]byte, why doesn't it convert automagically?
-		dblob, err := db.Get([]byte(txhash), nil)
-
-		if !ok {
+		blob, err := store.DB.Get(txhash[:], nil)
+		if err != nil {
 			http.Error(w, fmt.Sprintf("Transaction %s does not contain data", hex), http.StatusNotFound)
 			return
 		}
 
-//		if _, err := w.Write(blob); err != nil {
-		if _, err := w.Write(dblob); err != nil {
+		if _, err := w.Write(blob); err != nil {
 			log.Println(err)
 		}
 	case http.MethodPost:
@@ -122,21 +99,29 @@ func (store *Store) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		// should there be any validation of encryption/signatures - maybe a job that runs once a day and removes all keys,
 		// for  which there is no transaction on blockchain.
 		// Eventually someone would be paying for the server, so maybe have throttling for IPs and whitelist paid users (gateways)
-		store.mu.Lock()
-		_, exists := store.blobs[txhash]
-		if !exists {
-			store.blobs[txhash] = data
-			//alternative db installation
-			err = db.Put([]byte(txhash), []byte(data), nil)
-			if err { http.Error(w, "Some DB error", http.StatusConflict) }
-		}
-		store.mu.Unlock()
 
-		if exists {
+		tx, err := store.DB.OpenTransaction()
+		if err != nil {
+			http.Error(w, "Problem starting DB transaction.", http.StatusInternalServerError)
+			return
+		}
+		defer tx.Discard()
+
+		if _, err := tx.Get(txhash[:], nil); err != leveldb.ErrNotFound {
 			http.Error(w, "Transaction hash already stores data", http.StatusConflict)
+			return
 		}
 
-	}
+		if err := tx.Put(txhash[:], data, nil); err != nil {
+			log.Println(err)
+			http.Error(w, "Error saving to DB.", http.StatusInternalServerError)
+			return
+		}
 
+		if err := tx.Commit(); err != nil {
+			log.Println(err)
+			http.Error(w, "Error committing to DB.", http.StatusInternalServerError)
+		}
+	}
 
 }
